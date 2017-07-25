c40d67fbdccf10e66e632ddbff7dc38f348bc273
huangyuxiang <helloworldyx@gmail.com>
2017-07-25 17:20:42 +0800

    完善 MainActivity调用EmoticonRainView

--- app/src/main/java/com/nope/hyx/MainActivity.java
+++ app/src/main/java/com/nope/hyx/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,12 +23,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int dp50Pixel = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+
         mRainView = (EmoticonRainView) findViewById(R.id.emoticon_rain_view);
+        mRainView.setAutoRecycleBitmap(true);
         btnStartRain = (Button) findViewById(R.id.btn_start_rain);
         btnStartRain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EmoticonRainView.Conf conf = new EmoticonRainView.Conf.Builder().bitmaps(getBitmaps()).build();
+                EmoticonRainView.Conf conf = new EmoticonRainView.Conf.Builder().bitmaps(getBitmaps())
+                        .emoticonHeightPixel(dp50Pixel).emoticonWidthPixel(dp50Pixel).build();
                 mRainView.start(conf);
             }
         });
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     public List<Bitmap> getBitmaps(){
         List<Bitmap> bitmaps = new ArrayList<>();
+        //TODO 此处应自行对图片宽高进行压缩，避免OOM
         bitmaps.add(BitmapFactory.decodeResource(getResources(), R.drawable.baby));
         bitmaps.add(BitmapFactory.decodeResource(getResources(), R.drawable.paper));
         return bitmaps;
